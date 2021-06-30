46f44e47d3caf49b6c5e1b3d509b7ff78efcdff1
Dmytro Shcherba <dscherba76@gmail.com>
2021-06-30 21:59:11 +0300

    added lesson 2 code

--- project_1/src/components/Calculator.vue
+++ project_1/src/components/Calculator.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="container">
-    <input type="number" v-model="operand" /> = {{ result }}
-    <br />
-    <button @click="accumulate('+')">+</button>
-    <button @click="accumulate('-')">-</button>
-    <button @click="accumulate('*')">*</button>
-    <button @click="accumulate('/')">/</button>
-    <button @click="accumulate('**')">**</button>
-    <button @click="accumulate('//')">//</button>
-    <button @click="calculate()">=</button>
-    <button @click="reset()">C</button>
+    <div class="display">
+      <input v-model.number="operand1" />
+      <input v-model.number="operand2" />
+      = {{ result }}
+    </div>
+
+    <div v-show="error">Ошибка! {{ error }}</div>
+
+    <div class="keyboard">
+      <button
+        v-for="operand in operations"
+        :key="operand"
+        :disabled="operand1 === '' || operand2 === ''"
+        @click="calculate(operand)"
+      >
+        {{ operand }}
+      </button>
+    </div>
   </div>
 </template>
 
@@ -19,52 +27,67 @@ export default {
 
   data() {
     return {
-      operand: 0,
-      accumulator: 0,
-      operation: null,
-      result: null,
+      operations: ["+", "-", "/", "*", "//", "**"],
+      operand1: 0,
+      operand2: 0,
+      result: 0,
+      error: "",
     };
   },
 
   methods: {
-    accumulate(calcOperation) {
-      this.operation = calcOperation;
-      this.accumulator =
-        this.accumulator === 0 ? Number(this.operand) : this.accumulator;
-      this.operand = "";
+    add() {
+      this.result = this.operand1 + this.operand2;
+    },
+    substract() {
+      this.result = this.operand1 - this.operand2;
+    },
+    divide() {
+      const { operand1, operand2 } = this;
+      if (operand2 === 0) {
+        this.error = "Делить на 0 нельзя";
+      } else {
+        this.result = operand1 / operand2;
+      }
+    },
+    multiply() {
+      this.result = this.operand1 * this.operand2;
     },
 
-    calculate() {
-      this.operand = Number(this.operand);
-      switch (this.operation) {
+    intDivide() {
+      const { operand1, operand2 } = this;
+      if (operand2 === 0) {
+        this.error = "Делить на 0 нельзя";
+      } else {
+        this.result = Math.floor(operand1 / operand2);
+      }
+    },
+    power() {
+      this.result = this.operand1 ** this.operand2;
+    },
+
+    calculate(operation = "+") {
+      this.error = "";
+      switch (operation) {
         case "+":
-          this.accumulator += this.operand;
+          this.add();
           break;
         case "-":
-          this.accumulator -= this.operand;
+          this.substract();
           break;
         case "*":
-          this.accumulator *= this.operand;
+          this.multiply();
           break;
         case "/":
-          this.accumulator /= this.operand;
-          break;
-        case "**":
-          this.accumulator **= this.operand;
+          this.divide();
           break;
         case "//":
-          this.accumulator = Math.floor(this.accumulator / this.operand);
+          this.intDivide();
+          break;
+        case "**":
+          this.power();
           break;
       }
-      this.result = this.accumulator;
-      this.operand = "";
-    },
-
-    reset() {
-      this.operand = 0;
-      this.accumulator = 0;
-      this.operation = null;
-      this.result = null;
     },
   },
 };
